1bb671113baf3ab0fb3a3d6bcce54a87e3e375a4
VictorFil67 <victorfilinovych@gmail.com>
2023-12-08 12:08:12 +0200

    2-1 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { RewiewsWidget } from './RewiewsWidget/RewiewsWidget';
+// import { Statistics } from './Statistics/Statistics';
 export const App = () => {
   return (
     <div
@@ -7,10 +9,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <RewiewsWidget />
     </div>
   );
 };
